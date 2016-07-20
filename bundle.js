7dc4ecd851d3778e7ce430cf42e24178cc6378e1
Will Moody <williamcmoody@gmail.com>
2016-07-20 15:11:43 -0700

    many components implemented, grid built and clickable, initial state generation and basic state manipulation

--- bundle.js
+++ bundle.js
@@ -64,9 +64,48 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	console.log("loaded");
+	__webpack_require__(159);
 
 	var target = document.getElementById('root');
+
+	var booted = false;
+
+	var cellStates = [];
+	var rowStates = [];
+
+	if (!booted) {
+	  //if the page is just loading, generate a random state for all 2500 (50 rows, 50 columns) of cells
+	  var getRandomState = function getRandomState() {
+	    if (Math.floor(Math.random() * (1 - 0 + 1)) + 0) {
+	      return true;
+	    } else {
+	      return false;
+	    }
+	  };
+	  for (var i = 0; i < 2500; i++) {
+	    cellStates.push({
+	      key: i,
+	      alive: getRandomState()
+	    });
+	  }
+
+	  booted = true;
+	};
+
+	var buildRows = function buildRows() {
+	  var rows = [];
+	  var rowIndex = 0;
+	  for (var _i = 0; _i < 50; _i++) {
+
+	    var thisRow = [];
+	    for (var j = 0; j < 50; j++) {
+	      thisRow.push(cellStates[rowIndex * 50 + j]);
+	    }
+	    rows.push(_react2.default.createElement(CellRow, { cells: thisRow, key: _i }));
+	    rowIndex++;
+	  }
+	  rowStates = rows;
+	};
 
 	var Layout = function (_React$Component) {
 	  _inherits(Layout, _React$Component);
@@ -81,9 +120,9 @@
 	    key: 'render',
 	    value: function render() {
 	      return _react2.default.createElement(
-	        'h1',
-	        null,
-	        'Hello World'
+	        'div',
+	        { className: 'container' },
+	        _react2.default.createElement(Life, null)
 	      );
 	    }
 	  }]);
@@ -93,7 +132,152 @@
 
 	;
 
-	_reactDom2.default.render(_react2.default.createElement(Layout, null), target);
+	var Life = function (_React$Component2) {
+	  _inherits(Life, _React$Component2);
+
+	  function Life() {
+	    _classCallCheck(this, Life);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Life).apply(this, arguments));
+	  }
+
+	  _createClass(Life, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(Grid, null);
+	    }
+	  }]);
+
+	  return Life;
+	}(_react2.default.Component);
+
+	;
+
+	var Grid = function (_React$Component3) {
+	  _inherits(Grid, _React$Component3);
+
+	  function Grid() {
+	    _classCallCheck(this, Grid);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Grid).apply(this, arguments));
+	  }
+
+	  _createClass(Grid, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'table',
+	        null,
+	        _react2.default.createElement(CellMap, null)
+	      );
+	    }
+	  }]);
+
+	  return Grid;
+	}(_react2.default.Component);
+
+	;
+
+	var CellMap = function (_React$Component4) {
+	  _inherits(CellMap, _React$Component4);
+
+	  function CellMap() {
+	    _classCallCheck(this, CellMap);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(CellMap).apply(this, arguments));
+	  }
+
+	  _createClass(CellMap, [{
+	    key: 'render',
+	    value: function render() {
+	      buildRows();
+	      return _react2.default.createElement(
+	        'tbody',
+	        null,
+	        rowStates.map(function (row) {
+	          return row;
+	        })
+	      );
+	    }
+	  }]);
+
+	  return CellMap;
+	}(_react2.default.Component);
+
+	;
+
+	var CellRow = function (_React$Component5) {
+	  _inherits(CellRow, _React$Component5);
+
+	  function CellRow() {
+	    _classCallCheck(this, CellRow);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(CellRow).apply(this, arguments));
+	  }
+
+	  _createClass(CellRow, [{
+	    key: 'render',
+	    value: function render() {
+	      return _react2.default.createElement(
+	        'tr',
+	        null,
+	        this.props.cells.map(function (cell) {
+	          return _react2.default.createElement(Cell, { alive: cell.alive, key: cell.key, index: cell.key });
+	        })
+	      );
+	    }
+	  }]);
+
+	  return CellRow;
+	}(_react2.default.Component);
+
+	var Cell = function (_React$Component6) {
+	  _inherits(Cell, _React$Component6);
+
+	  function Cell() {
+	    _classCallCheck(this, Cell);
+
+	    return _possibleConstructorReturn(this, Object.getPrototypeOf(Cell).apply(this, arguments));
+	  }
+
+	  _createClass(Cell, [{
+	    key: 'render',
+	    value: function render() {
+	      var _this7 = this;
+
+	      var handleClick = function handleClick() {
+	        cellStates[_this7.props.index].alive = !cellStates[_this7.props.index].alive;
+	        update();
+	      };
+	      if (this.props.alive) {
+	        return _react2.default.createElement('th', { className: 'liveCell', onClick: function onClick() {
+	            return handleClick();
+	          }, onMouseUp: function onMouseUp() {
+	            return handleClick();
+	          } });
+	      } else {
+	        return _react2.default.createElement('th', { className: 'deadCell', onClick: function onClick() {
+	            return handleClick();
+	          }, onMouseUp: function onMouseUp() {
+	            return handleClick();
+	          } });
+	      };
+	    }
+	  }]);
+
+	  return Cell;
+	}(_react2.default.Component);
+
+	;
+
+	var lifeCycle = function lifeCycle() {};
+
+	var update = function update() {
+	  console.log("updated");
+	  _reactDom2.default.render(_react2.default.createElement(Layout, null), target);
+	};
+
+	update();
 
 /***/ },
 /* 1 */
@@ -19726,6 +19910,354 @@
 	'use strict';
 
 	module.exports = __webpack_require__(3);
+
+
+/***/ },
+/* 159 */
+/***/ function(module, exports, __webpack_require__) {
+
+	// style-loader: Adds some css to the DOM by adding a <style> tag
+
+	// load the styles
+	var content = __webpack_require__(160);
+	if(typeof content === 'string') content = [[module.id, content, '']];
+	// add the styles to the DOM
+	var update = __webpack_require__(162)(content, {});
+	if(content.locals) module.exports = content.locals;
+	// Hot Module Replacement
+	if(false) {
+		// When the styles change, update the <style> tags
+		if(!content.locals) {
+			module.hot.accept("!!./../node_modules/css-loader/index.js!./../node_modules/sass-loader/index.js!./master.sass", function() {
+				var newContent = require("!!./../node_modules/css-loader/index.js!./../node_modules/sass-loader/index.js!./master.sass");
+				if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
+				update(newContent);
+			});
+		}
+		// When the module is disposed, remove the <style> tags
+		module.hot.dispose(function() { update(); });
+	}
+
+/***/ },
+/* 160 */
+/***/ function(module, exports, __webpack_require__) {
+
+	exports = module.exports = __webpack_require__(161)();
+	// imports
+
+
+	// module
+	exports.push([module.id, "body {\n  background-color: #424242; }\n\n.container {\n  padding-right: 15px;\n  padding-left: 15px;\n  margin-right: auto;\n  margin-left: auto;\n  position: relative; }\n\ntable {\n  width: 250px;\n  margin-left: auto;\n  margin-right: auto; }\n\nth {\n  min-width: 13px;\n  height: 13px; }\n\n.liveCell {\n  background-color: #7cb342; }\n\n.deadCell {\n  background-color: #212121; }\n\n@media (min-width: 768px) {\n  .container {\n    width: 750px; } }\n\n@media (min-width: 992px) {\n  .container {\n    width: 970px; } }\n\n@media (min-width: 1200px) {\n  .container {\n    width: 1170px; } }\n", ""]);
+
+	// exports
+
+
+/***/ },
+/* 161 */
+/***/ function(module, exports) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	// css base code, injected by the css-loader
+	module.exports = function() {
+		var list = [];
+
+		// return the list of modules as css string
+		list.toString = function toString() {
+			var result = [];
+			for(var i = 0; i < this.length; i++) {
+				var item = this[i];
+				if(item[2]) {
+					result.push("@media " + item[2] + "{" + item[1] + "}");
+				} else {
+					result.push(item[1]);
+				}
+			}
+			return result.join("");
+		};
+
+		// import a list of modules into the list
+		list.i = function(modules, mediaQuery) {
+			if(typeof modules === "string")
+				modules = [[null, modules, ""]];
+			var alreadyImportedModules = {};
+			for(var i = 0; i < this.length; i++) {
+				var id = this[i][0];
+				if(typeof id === "number")
+					alreadyImportedModules[id] = true;
+			}
+			for(i = 0; i < modules.length; i++) {
+				var item = modules[i];
+				// skip already imported module
+				// this implementation is not 100% perfect for weird media query combinations
+				//  when a module is imported multiple times with different media queries.
+				//  I hope this will never occur (Hey this way we have smaller bundles)
+				if(typeof item[0] !== "number" || !alreadyImportedModules[item[0]]) {
+					if(mediaQuery && !item[2]) {
+						item[2] = mediaQuery;
+					} else if(mediaQuery) {
+						item[2] = "(" + item[2] + ") and (" + mediaQuery + ")";
+					}
+					list.push(item);
+				}
+			}
+		};
+		return list;
+	};
+
+
+/***/ },
+/* 162 */
+/***/ function(module, exports, __webpack_require__) {
+
+	/*
+		MIT License http://www.opensource.org/licenses/mit-license.php
+		Author Tobias Koppers @sokra
+	*/
+	var stylesInDom = {},
+		memoize = function(fn) {
+			var memo;
+			return function () {
+				if (typeof memo === "undefined") memo = fn.apply(this, arguments);
+				return memo;
+			};
+		},
+		isOldIE = memoize(function() {
+			return /msie [6-9]\b/.test(window.navigator.userAgent.toLowerCase());
+		}),
+		getHeadElement = memoize(function () {
+			return document.head || document.getElementsByTagName("head")[0];
+		}),
+		singletonElement = null,
+		singletonCounter = 0,
+		styleElementsInsertedAtTop = [];
+
+	module.exports = function(list, options) {
+		if(false) {
+			if(typeof document !== "object") throw new Error("The style-loader cannot be used in a non-browser environment");
+		}
+
+		options = options || {};
+		// Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
+		// tags it will allow on a page
+		if (typeof options.singleton === "undefined") options.singleton = isOldIE();
+
+		// By default, add <style> tags to the bottom of <head>.
+		if (typeof options.insertAt === "undefined") options.insertAt = "bottom";
+
+		var styles = listToStyles(list);
+		addStylesToDom(styles, options);
+
+		return function update(newList) {
+			var mayRemove = [];
+			for(var i = 0; i < styles.length; i++) {
+				var item = styles[i];
+				var domStyle = stylesInDom[item.id];
+				domStyle.refs--;
+				mayRemove.push(domStyle);
+			}
+			if(newList) {
+				var newStyles = listToStyles(newList);
+				addStylesToDom(newStyles, options);
+			}
+			for(var i = 0; i < mayRemove.length; i++) {
+				var domStyle = mayRemove[i];
+				if(domStyle.refs === 0) {
+					for(var j = 0; j < domStyle.parts.length; j++)
+						domStyle.parts[j]();
+					delete stylesInDom[domStyle.id];
+				}
+			}
+		};
+	}
+
+	function addStylesToDom(styles, options) {
+		for(var i = 0; i < styles.length; i++) {
+			var item = styles[i];
+			var domStyle = stylesInDom[item.id];
+			if(domStyle) {
+				domStyle.refs++;
+				for(var j = 0; j < domStyle.parts.length; j++) {
+					domStyle.parts[j](item.parts[j]);
+				}
+				for(; j < item.parts.length; j++) {
+					domStyle.parts.push(addStyle(item.parts[j], options));
+				}
+			} else {
+				var parts = [];
+				for(var j = 0; j < item.parts.length; j++) {
+					parts.push(addStyle(item.parts[j], options));
+				}
+				stylesInDom[item.id] = {id: item.id, refs: 1, parts: parts};
+			}
+		}
+	}
+
+	function listToStyles(list) {
+		var styles = [];
+		var newStyles = {};
+		for(var i = 0; i < list.length; i++) {
+			var item = list[i];
+			var id = item[0];
+			var css = item[1];
+			var media = item[2];
+			var sourceMap = item[3];
+			var part = {css: css, media: media, sourceMap: sourceMap};
+			if(!newStyles[id])
+				styles.push(newStyles[id] = {id: id, parts: [part]});
+			else
+				newStyles[id].parts.push(part);
+		}
+		return styles;
+	}
+
+	function insertStyleElement(options, styleElement) {
+		var head = getHeadElement();
+		var lastStyleElementInsertedAtTop = styleElementsInsertedAtTop[styleElementsInsertedAtTop.length - 1];
+		if (options.insertAt === "top") {
+			if(!lastStyleElementInsertedAtTop) {
+				head.insertBefore(styleElement, head.firstChild);
+			} else if(lastStyleElementInsertedAtTop.nextSibling) {
+				head.insertBefore(styleElement, lastStyleElementInsertedAtTop.nextSibling);
+			} else {
+				head.appendChild(styleElement);
+			}
+			styleElementsInsertedAtTop.push(styleElement);
+		} else if (options.insertAt === "bottom") {
+			head.appendChild(styleElement);
+		} else {
+			throw new Error("Invalid value for parameter 'insertAt'. Must be 'top' or 'bottom'.");
+		}
+	}
+
+	function removeStyleElement(styleElement) {
+		styleElement.parentNode.removeChild(styleElement);
+		var idx = styleElementsInsertedAtTop.indexOf(styleElement);
+		if(idx >= 0) {
+			styleElementsInsertedAtTop.splice(idx, 1);
+		}
+	}
+
+	function createStyleElement(options) {
+		var styleElement = document.createElement("style");
+		styleElement.type = "text/css";
+		insertStyleElement(options, styleElement);
+		return styleElement;
+	}
+
+	function createLinkElement(options) {
+		var linkElement = document.createElement("link");
+		linkElement.rel = "stylesheet";
+		insertStyleElement(options, linkElement);
+		return linkElement;
+	}
+
+	function addStyle(obj, options) {
+		var styleElement, update, remove;
+
+		if (options.singleton) {
+			var styleIndex = singletonCounter++;
+			styleElement = singletonElement || (singletonElement = createStyleElement(options));
+			update = applyToSingletonTag.bind(null, styleElement, styleIndex, false);
+			remove = applyToSingletonTag.bind(null, styleElement, styleIndex, true);
+		} else if(obj.sourceMap &&
+			typeof URL === "function" &&
+			typeof URL.createObjectURL === "function" &&
+			typeof URL.revokeObjectURL === "function" &&
+			typeof Blob === "function" &&
+			typeof btoa === "function") {
+			styleElement = createLinkElement(options);
+			update = updateLink.bind(null, styleElement);
+			remove = function() {
+				removeStyleElement(styleElement);
+				if(styleElement.href)
+					URL.revokeObjectURL(styleElement.href);
+			};
+		} else {
+			styleElement = createStyleElement(options);
+			update = applyToTag.bind(null, styleElement);
+			remove = function() {
+				removeStyleElement(styleElement);
+			};
+		}
+
+		update(obj);
+
+		return function updateStyle(newObj) {
+			if(newObj) {
+				if(newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap)
+					return;
+				update(obj = newObj);
+			} else {
+				remove();
+			}
+		};
+	}
+
+	var replaceText = (function () {
+		var textStore = [];
+
+		return function (index, replacement) {
+			textStore[index] = replacement;
+			return textStore.filter(Boolean).join('\n');
+		};
+	})();
+
+	function applyToSingletonTag(styleElement, index, remove, obj) {
+		var css = remove ? "" : obj.css;
+
+		if (styleElement.styleSheet) {
+			styleElement.styleSheet.cssText = replaceText(index, css);
+		} else {
+			var cssNode = document.createTextNode(css);
+			var childNodes = styleElement.childNodes;
+			if (childNodes[index]) styleElement.removeChild(childNodes[index]);
+			if (childNodes.length) {
+				styleElement.insertBefore(cssNode, childNodes[index]);
+			} else {
+				styleElement.appendChild(cssNode);
+			}
+		}
+	}
+
+	function applyToTag(styleElement, obj) {
+		var css = obj.css;
+		var media = obj.media;
+
+		if(media) {
+			styleElement.setAttribute("media", media)
+		}
+
+		if(styleElement.styleSheet) {
+			styleElement.styleSheet.cssText = css;
+		} else {
+			while(styleElement.firstChild) {
+				styleElement.removeChild(styleElement.firstChild);
+			}
+			styleElement.appendChild(document.createTextNode(css));
+		}
+	}
+
+	function updateLink(linkElement, obj) {
+		var css = obj.css;
+		var sourceMap = obj.sourceMap;
+
+		if(sourceMap) {
+			// http://stackoverflow.com/a/26603875
+			css += "\n/*# sourceMappingURL=data:application/json;base64," + btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))) + " */";
+		}
+
+		var blob = new Blob([css], { type: "text/css" });
+
+		var oldSrc = linkElement.href;
+
+		linkElement.href = URL.createObjectURL(blob);
+
+		if(oldSrc)
+			URL.revokeObjectURL(oldSrc);
+	}
 
 
 /***/ }
--- js/main.js
+++ js/main.js
@@ -1,16 +1,122 @@
+require('../css/master.sass');
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-console.log("loaded");
-
 const target = document.getElementById('root');
+
+let booted = false;
+
+let cellStates = [];
+let rowStates = [];
+
+if (!booted) {
+//if the page is just loading, generate a random state for all 2500 (50 rows, 50 columns) of cells
+  let getRandomState = function(){
+    if (Math.floor(Math.random() * (1 - 0 + 1)) + 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  for (let i = 0; i < 2500; i++) {
+    cellStates.push({
+      key: i,
+      alive: (getRandomState())
+    })
+  }
+
+booted = true;
+};
+
+let buildRows = () => {
+ let rows = [];
+ let rowIndex = 0
+ for (let i = 0; i < 50; i++){
+
+   let thisRow = []
+   for (let j = 0; j < 50; j++){
+     thisRow.push(cellStates[((rowIndex * 50) + j)]);
+   }
+   rows.push(<CellRow cells={thisRow} key={i}/>)
+   rowIndex++;
+ }
+ rowStates = rows;
+}
 
 class Layout extends React.Component {
   render(){
     return(
-      <h1>Hello World</h1>
+      <div className="container">
+        <Life />
+      </div>
     )
   }
 };
 
-ReactDOM.render(<Layout />, target);
+class Life extends React.Component {
+  render(){
+    return(
+      <Grid />
+    )
+  }
+};
+
+class Grid extends React.Component {
+  render(){
+    return(
+      <table>
+        <CellMap />
+      </table>
+    )
+  }
+};
+
+class CellMap extends React.Component {
+  render(){
+    buildRows();
+    return(
+      <tbody>
+        {rowStates.map(row => row)}
+      </tbody>
+    )
+  }
+};
+class CellRow extends React.Component {
+  render(){
+    return(
+      <tr>{this.props.cells.map(cell => <Cell alive={cell.alive} key={cell.key} index={cell.key}/>)}</tr>
+    )
+  }
+}
+class Cell extends React.Component {
+  render(){
+    let handleClick = () => {
+      cellStates[this.props.index].alive = (!cellStates[this.props.index].alive);
+      update();
+    }
+    if (this.props.alive) {
+      return (
+        <th className="liveCell" onClick={() => handleClick()} onMouseUp={() => handleClick()}></th>
+      )
+    } else {
+      return (
+        <th className="deadCell" onClick={() => handleClick()} onMouseUp={() => handleClick()}></th>
+      )
+    };
+
+  };
+};
+
+let lifeCycle = function(){
+
+}
+
+let update = function(){
+  console.log("updated");
+  ReactDOM.render(<Layout />, target);
+};
+
+
+update();
